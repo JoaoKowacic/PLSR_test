128c5d801b4938c5d59732c5e25638a85334e6bd
joao kowacic <joao.kowacic@gmail.com>
2024-01-10 01:10:24 -0300

    adicionado barra de filtro

--- marketplace/src/components/ProductList.js
+++ marketplace/src/components/ProductList.js
@@ -8,18 +8,32 @@ class ProductList extends Component {
   
       this.state = {
         products: [],
+        filter: ''
       }
     }
   
     componentDidMount() {
       this.setState({ products: data });
     }
+
+    handleFilterChange = (event) => {
+      this.setState({ filter: event.target.value });
+    }
   
     render() {
+      const sports = [...new Set(this.state.products.map(product => product.sport))];
+      const filteredProducts = this.state.filter ? this.state.products.filter(product => product.sport === this.state.filter) : this.state.products;
+      
       return (
         <div className="container main-content">
+          <select value={this.state.filter} onChange={this.handleFilterChange}>
+            <option value="">All</option>
+            {sports.map((sport, index) => (
+              <option key={index} value={sport}> {sport} </option>
+            ))}
+          </select>
           {
-            this.state.products.map((product, index) => {
+            filteredProducts.map((product, index) => {
               return <ProductRow key={index} image={product.image_url} name={product.name} details={product.details} price={product.price} />
             })
           }
